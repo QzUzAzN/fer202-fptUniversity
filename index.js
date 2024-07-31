537aec50aea15797200810b1ed95ebc2f2f4f9f5
Quan Hoang <mailemisoquan@gmail.com>
2024-07-31 15:31:15 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const hostname = "localhost"; //127.0.01
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("content-Typpe", "text/plain");
+  res.end("Hello FPT University");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
